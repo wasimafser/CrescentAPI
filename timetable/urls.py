be21477e0d511cb9404e7596a89f9f727be1cce1
wasimafser <wasim.afser@gmail.com>
2019-10-06 19:58:58 +0530

    urls error with no views

--- timetable/urls.py
+++ timetable/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^first/$', views.first_page_view, name='first_page'),
-    url(r'^test/$',views.testview, name='testpage')
+
 ]
