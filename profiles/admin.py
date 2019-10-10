3b67450e9497ea90f7a136fa86a02c6729c4dfe5
wasimafser <wasim.afser@gmail.com>
2019-10-10 13:35:40 +0530

    Add static folder

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+admin.site.register(models.Branch)
+admin.site.register(models.Department)
+admin.site.register(models.Designation)
+admin.site.register(models.Profile)
--- profiles/models.py
+++ profiles/models.py
@@ -1,4 +1,52 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+
+class Designation(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Branch(models.Model):
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    account_id = models.IntegerField(null=True, blank=True)
+    full_name = models.CharField(max_length=100, null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    gender_choice = (
+        ('male', 'Male'),
+        ('female', 'Female')
+    )
+    gender = models.CharField(max_length=10, choices=gender_choice, null=True, blank=True)
+    other_email = models.CharField(max_length=100, null=True, blank=True)
+    phone = models.CharField(max_length=100, null=True, blank=True)
+    designation = models.ForeignKey(Designation, on_delete=models.CASCADE, related_name='+', null=True, blank=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='+', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.full_name}"
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
