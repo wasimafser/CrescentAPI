30266fa4582ed78521a1fca4b7ce1d7d2abd883b
ThanveerNawaz <thanveernawaz094@gmail.com>
2019-10-10 13:38:11 +0530

    initial commit

--- timetable/urls.py
+++ timetable/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import include, url
-from . import views
+from django.conf.urls import url,include
+from .import views
 
 urlpatterns = [
-
+    url(r'^signin$',views.singinview,name='Signin')
 ]
--- timetable/views.py
+++ timetable/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def singinview(request):
+    return render(request, 'templates/timetable/signinhtml.html')
